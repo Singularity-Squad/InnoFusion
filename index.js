0c00fe88eae3977a6e207578755f740178cce13e
xpert0 <github.jaivs@slmail.me>
2024-07-27 23:46:15 +0530

    i

--- index.js
+++ index.js
@@ -42,27 +42,6 @@ fetch(csvFile)
       const card = document.createElement('div');
       card.classList.add('card');
       card.innerHTML = `
-        //const csvFile = "https://raw.githubusercontent.com/Singularity-Squad/InnoFusion/site/cards/data.csv"; //for localhost
-const csvFile = "data.csv"; //for server
-
-fetch(csvFile)
-  .then(response => response.text())
-  .then(data => {
-    const rows = data.split('\n');
-    const headers = rows[0].split(',');
-    const titleIndex = headers.indexOf('title');
-    const descriptionIndex = headers.indexOf('description');
-    const imgIndex = headers.indexOf('image');
-    const cardContainer = document.getElementById('card-container');
-
-    for (let i = 1; i < rows.length; i++) {
-      const row = rows[i].split(',');
-      const title = row[titleIndex];
-      const description = row[descriptionIndex];
-      const imgs = row[imgIndex];
-      const card = document.createElement('div');
-      card.classList.add('card');
-      card.innerHTML = `
         <h3><b><big>Available Coupons</big></b></h3>
         <div class="container" id="btn">
               <div class="coupon">
