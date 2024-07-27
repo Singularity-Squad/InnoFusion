83e536c467e031dae8ab1116072ac04ba5fa8134
xpert0 <github.jaivs@slmail.me>
2024-07-27 16:08:06 +0530

    cards

--- cards/cards.js
+++ cards/cards.js
@@ -1,4 +1,4 @@
-const csvFile = 'data.csv'; // Replace with your CSV file path
+const csvFile = "https://raw.githubusercontent.com/Singularity-Squad/InnoFusion/site/cards/data.csv"; // Replace with your CSV file path
 
 fetch(csvFile)
   .then(response => response.text())
