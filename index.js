fe8be5da2eb0de87d453be547cbd6d561f4c882f
xpert0 <github.jaivs@slmail.me>
2024-07-27 23:58:01 +0530

    jsfix

--- index.js
+++ index.js
@@ -20,34 +20,3 @@ function close_popup()
 document.querySelector("#buy").addEventListener("click",()=> {
     window.scrollTo(0,document.scrollHeight);
 })
-
-//const csvFile = "https://raw.githubusercontent.com/Singularity-Squad/InnoFusion/site/cards/data.csv"; //for localhost
-const csvFile = "data.csv"; //for server
-
-fetch(csvFile)
-  .then(response => response.text())
-  .then(data => {
-    const rows = data.split('\n');
-    const headers = rows[0].split(',');
-    const titleIndex = headers.indexOf('title');
-    const descriptionIndex = headers.indexOf('description');
-    const imgIndex = headers.indexOf('image');
-    const cardContainer = document.getElementById('card-container');
-
-    for (let i = 1; i < rows.length; i++) {
-      const row = rows[i].split(',');
-      const title = row[titleIndex];
-      const description = row[descriptionIndex];
-      const imgs = row[imgIndex];
-      const card = document.createElement('div');
-      card.classList.add('card');
-      card.innerHTML = `
-        <h1>${title}</h1>
-        <p>${description}</p>
-        <img src=${imgs}>
-      `;
-      cardContainer.appendChild(card);
-    }
-  })
-
-  .catch(error => console.error('Error:', error));
