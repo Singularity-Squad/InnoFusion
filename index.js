03492cb403283e6d08ac43275921cee5f6793bf5
xpert0 <github.jaivs@slmail.me>
2024-07-28 13:46:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 var blury= document.getElementById("btn");
-// var blur1= document.getElementById("btn1");
-// var blur2= document.getElementById("btn2");
 var popup = document.getElementById("popup");
 var code=document.getElementById("ok");
 function add()
 {
     blury.classList.add("blury");
-    // blur1.classList.add("blury");
-    // blur2.classList.add("blury");
     popup.classList.add("open");
 
 }
@@ -15,7 +11,4 @@ function close_popup()
 {
     popup.classList.remove("open");
     blury.classList.remove("blury");
-    // blur1.classList.remove("blury");
-    // blur2.classList.remove("blury");
-    
 }
