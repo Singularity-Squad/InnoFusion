576f42a4698385909a32627df91af846251ff3de
Apugem <141444132+Apugem@users.noreply.github.com>
2024-07-28 12:49:32 +0530

    Full Push

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-// import { cors } from "cors";
 var blury= document.getElementById("btn");
+// var blur1= document.getElementById("btn1");
+// var blur2= document.getElementById("btn2");
 var popup = document.getElementById("popup");
+var code=document.getElementById("ok");
 function add()
 {
     blury.classList.add("blury");
+    // blur1.classList.add("blury");
+    // blur2.classList.add("blury");
     popup.classList.add("open");
 
 }
@@ -11,32 +15,7 @@ function close_popup()
 {
     popup.classList.remove("open");
     blury.classList.remove("blury");
+    // blur1.classList.remove("blury");
+    // blur2.classList.remove("blury");
     
-}   
-
-function sellsub()
-{
-    const form = document.getElementById('sell-form');
-
-        form.addEventListener('submit', (event) => {
-            event.preventDefault();
-
-            const platform = document.getElementById('platform').value;
-            const code = document.getElementById('code').value;
-            const price = document.getElementById('price').value;
-            console.log("data updated")
-
-            // Create a CSV string
-            const csvData = `${code},${price},${platform},${expd},${email}\n`;
-            console.log("CSV stuctured")
-
-            // Append to local storage (not ideal for production)
-            const existingData = localStorage.getItem("data.csv") || '';
-            localStorage.setItem("data.csv", existingData + csvData);
-            console.log("Local Storage Updated")
-
-            // Display a success message or clear form fields
-            alert('Data saved successfully!');
-            // form.reset();
-        });
-    }
+}
