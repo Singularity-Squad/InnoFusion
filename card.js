a78cd93b315bda0950ce4c5b34bae822288efc7c
xpert0 <github.jaivs@slmail.me>
2024-07-29 11:04:12 +0530

    script

--- card.js
+++ card.js
@@ -19,60 +19,19 @@ fetch(csvUrl)
             const content = document.createElement('div');
             content.classList.add('card-content');
             content.innerHTML = `
-                  <link rel="stylesheet" href="index.css">
-                  <style>
-                    .left {
-                      width: 80px;
-                      border-right: 2px dashed rgba(0, 0, 0, 0.15);
-                    }
- 
-                    .left div {
-                      transform: rotate(-90deg);
-                      white-space: nowrap;
-                      font-weight: bold;
-                    }
- 
- .center {
-   flex-grow: 1;
-   text-align: center;
- }
- 
- .right {
-   width: 140px;
-   background-image: radial-gradient(circle at 100% 50%, transparent 25px, #fff 26px);
-   
- }
- 
- .right a{
-   font-family: 'Libre Barcode 128 Text', cursive;
-   font-size: 1rem;
-   padding-right:1.5rem;
-   cursor: pointer;
- }
-
- .center h3 {
-   font-size: 2.15rem;
- }
- 
- .center small {
-   font-size: 0.625rem;
-   font-weight: 600;
-   letter-spacing: 2px;
- }
-                  </style>
-                  <div class="left">
-                    <div>Flipkart</div>
-                  </div>
-                  <div class="center">
-                    <div>
-                      <img src="logo12.png" id="resize">
-                      <h3>Flat 40% OFF</h3>
-                      <small>Valid until May 2023</small>
-                    </div>
-                  </div>
-                  <div class="right">
-                    <a id="code" onclick="add()">Get Code</a>
-                  </div>`
+              <div class="left">
+                <div>Flipkart</div>
+              </div>
+              <div class="center">
+                <div>
+                  <img src="logo12.png" id="resize">
+                  <h3>Flat 40% OFF</h3>
+                  <small>Valid until May 2023</small>
+                </div>
+              </div>
+              <div class="right">
+                <a id="code" onclick="add()">Get Code</a>
+              </div>`
             `;
             card.appendChild(content);
 
